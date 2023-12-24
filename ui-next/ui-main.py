03f1b5543f2b137d6b925d88f8d04416b75a0bd0
xd43vild69 <xd43vild69@gmail.com>
2023-12-24 13:49:57 -0500

    Add buttons to tab1

--- ui-next/ui-main.py
+++ ui-next/ui-main.py
@@ -9,9 +9,8 @@ def apply_dark_theme():
 
     # Configure the style for various elements
     style.theme_use('clam')  # Use 'clam' theme as a base
-    style.configure('.', background='#2E2E2E', foreground='#000000', fieldbackground='#2E2E2E')
+    style.configure('.', background='#1B1B1B', foreground='#353535', fieldbackground='#2E2E2E')
     style.map('.', background=[('disabled', '#2E2E2E'), ('selected', '#3E3E3E')])
-
 
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
@@ -22,12 +21,14 @@ root.style = ttk.Style(root)
 
 apply_dark_theme()
 
-
-
-
 def change_tab(event):
     selected_tab = notebook.index(notebook.select())
     print(f"Selected Tab: {selected_tab}")
+
+
+def cleanFiles():
+    print("Ok1")
+    return
 
 # Create a notebook (tabs container)
 notebook = ttk.Notebook(root)
@@ -39,17 +40,28 @@ tab3 = ttk.Frame(notebook)
 #tab4 = ttk.Frame(notebook)
 
 # Add tabs to the notebook
-notebook.add(tab1, text="N-A")
+notebook.add(tab1, text="Organization")
 notebook.add(tab2, text="Captioning")
 notebook.add(tab3, text="Lora setup")
 #notebook.add(tab4, text="Lora setup")
 
 # Add content to tabs
-label1 = tk.Label(tab1, text="Normalize && Aumentation")
-label1.pack(padx=10, pady=10)
+#label1 = tk.Label(tab1, text="Normalize")
+#label1.pack(padx=10, pady=10)
+5
+buttonNormalize = tk.Button(tab1, text="Normalize", command=cleanFiles)
+buttonNormalize.place(x=20, y=40)
 
-label2 = tk.Label(tab2, text="Pick the path to aumentation")
-label2.pack(padx=10, pady=10)
+buttonAugmentation = tk.Button(tab1, text="Augmentation", command=cleanFiles)
+buttonAugmentation.place(x=20, y=80)
+
+"""
+label2 = tk.Label(tab1, text="Aumentation")
+label2.pack(padx=10, pady=20)
+"""
+
+label3 = tk.Label(tab2, text="Pick the path to aumentation")
+label3.pack(padx=10, pady=10)
 
 """
 
